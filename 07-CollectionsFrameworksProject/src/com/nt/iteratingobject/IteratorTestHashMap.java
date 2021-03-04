5841ed073851bc56fdd92f0f2c4e64840ba52b08
Saikat Mondal <saikatmondal427@gmail.com>
2021-03-04 10:04:54 +0530

    new

---
+++ 07-CollectionsFrameworksProject/src/com/nt/iteratingobject/IteratorTestHashMap.java
@@ -0,0 +1,17 @@
+package com.nt.iteratingobject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class IteratorTestHashMap {
+
+	public static void main(String[] args) {
+		HashMap<Integer,Integer> hm=new HashMap<>();
+		for(int i=1;i<=10;i++) {
+			hm.put(i, i);
+		}
+	
+	}
+
+}
